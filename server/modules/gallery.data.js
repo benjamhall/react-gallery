55752b5d63d2a83f7f2b7d520c6a449f988c4e67
Ben Hall <benjamhall@gmail.com>
2021-06-04 18:14:47 -0400

    add crawfish image

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,6 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 2, path: 'images/crawfish.jpg', description: 'There is nothing quite like a Cajun Crawfish Boil', likes: 0 }
 ];
 
 module.exports = galleryItems;
