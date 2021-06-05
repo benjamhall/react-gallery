602ca0545945eae10c8f4a122733b3707055a496
Ben Hall <benjamhall@gmail.com>
2021-06-05 18:54:56 -0400

    update description of yosemite photo

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-    { id: 1, path: 'images/yosemite.jpg', description: 'One of my fav hiking places is Yosemite National Park.', likes: 0 },
+    { id: 1, path: 'images/yosemite.jpg', description: 'One of my favorite hiking places is Yosemite National Park.', likes: 0 },
     { id: 2, path: 'images/crawfish.jpg', description: 'There is nothing quite like a Cajun Crawfish Boil', likes: 0 },
     { id: 3, path: 'images/bike.jpg', description: 'Going mountain biking is one of my favorite forms of adventuring', likes: 0 }
 ];
