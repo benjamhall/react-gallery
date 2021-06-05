0e06f808ec8928aed2efaff5ba6a04fa4e37b23c
Ben Hall <benjamhall@gmail.com>
2021-06-05 16:04:25 -0400

    GalleryItem put request

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,18 +1,41 @@
-import './GalleryItem.css'
+import axios from 'axios';
+import './GalleryItem.css';
+import {useState} from 'react'
 
-function GalleryItem ({item}) {
+function GalleryItem ({item, getGalleryList}) {
 
-    //const []
+    const [liked, setLiked] = useState(0);
+
+    const handleLike = () => {
+        console.log('like button works', item);
+
+        const data = {
+            counter
+        }
+
+        axios.put(`/gallery/${item.id}`, data)
+        .then(response => {
+            console.log(response);
+            getGalleryList();
+        }).catch(err => {
+            console.log(err);
+        })
+
+    }
 
     return (
         <div className="listItem">
             <div>
                 {item.description}
                 <img src={item.path} />
+                <button onClick={handleLike}>Like</button>
+                <p>Likes:{liked} </p>
             </div>
+            <button onClick={() => setIsHidden(!isHidden)}></button>
+            
         </div>
     )
-}
+};
 
 
 export default GalleryItem;
